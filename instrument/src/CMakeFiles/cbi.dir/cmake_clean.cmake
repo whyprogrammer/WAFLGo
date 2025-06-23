3d5a8ac14f7faff62436bd2ddee906e8914f9313
whyprogrammer <hy_wang@hust.edu.cn>
2025-06-23 06:49:20 +0000

    test push

---
+++ instrument/src/CMakeFiles/cbi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/cbi"
+  "../bin/cbi.pdb"
+  "CMakeFiles/cbi.dir/cbi.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cbi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
